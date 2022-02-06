bd950c3a2ddfe18af9d634c77cd926ca8e6c902a
zhehui-wang <64948858+zhehui-wang@users.noreply.github.com>
2022-02-07 01:42:31 +0800

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,4 @@
+#pre-process VGG16 on CIFAR-10
+python EMOMC.py --working_mode 0 --network_type 2 --dataset_type 1 
+#trade-off between accuracy and energy on dataflow type no.3
+python EMOMC.py --working_mode 1 --network_type 2 --dataset_type 1 --dataflow 3
