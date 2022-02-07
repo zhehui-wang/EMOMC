6b646ae9977ff7c33cca8bc5db67c33dd0c01136
zhehui-wang <64948858+zhehui-wang@users.noreply.github.com>
2022-02-07 11:17:33 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 #pre-process VGG16 on CIFAR-10
 python EMOMC.py --working_mode 0 --network_type 2 --dataset_type 1 
-#trade-off between accuracy and energy on dataflow type no.30
+#trade-off between accuracy and energy on dataflow type X|Fx
 python EMOMC.py --working_mode 1 --network_type 2 --dataset_type 1 --dataflow_type 3
